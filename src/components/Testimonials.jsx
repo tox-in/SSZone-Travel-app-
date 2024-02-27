e889c1d7b362edb691746b9bde30842af4fa05bf
tox-in <habimanat78@gmail.com>
2024-02-27 11:32:12 +0200

    made important changes in this section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import avatarImage from "../assets/avatarImage.jpeg";
+import { users } from "../data/data";
 
 export default function Testimonials() {
   return (
@@ -8,16 +8,16 @@ export default function Testimonials() {
         <h2 className="text-3xl font-bold">Happy Customers</h2>
       </div>
       <div className="flex justify-center gap-8 mx-2">
-        {Array.from({ length: 3 }).map((_, index) => (
+        {/* Map over the array of users */}
+        {users.map((user, index) => (
           <div key={index} className="testimonial bg-white p-8 rounded-lg shadow-md transition-transform hover:scale-105">
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate asperiores eaque.
-            </p>
+            <p className="mb-4">{user.experience}</p>
             <div className="flex items-center">
-              <img src={avatarImage} alt="" className="w-12 h-12 rounded-full" />
+              <img src={user.image} alt="" className="w-12 h-12 rounded-full" />
               <div className="ml-4">
-                <h4 className="text-lg font-semibold">Kishan Sheth</h4>
-                <span className="text-sm">CEO - Shashaan Web Solutions</span>
+                <h4 className="text-lg font-semibold">{user.name}</h4>
+                <h4 className="text-lg font-semibold">{user.email}</h4>
+                <span className="text-sm">{user.role}</span>
               </div>
             </div>
           </div>
